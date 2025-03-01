f3669cae470015a6c9005cc1ae67bd1029f7f201
shivamjha296 <sjha13780@gmail.com>
2025-03-01 20:25:27 +0530

    Change 1 for deploy

--- rupvan/vite.config.ts
+++ rupvan/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  root: '.',  // Ensure Vite starts from the project root
+  build: {
+    outDir: 'dist',  // Output directory for Netlify
+    emptyOutDir: true,  // Clears the old build before a new one
+  }
 });
